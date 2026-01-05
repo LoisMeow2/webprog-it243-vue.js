cd700bea1a881a1ee34a8fedcae5e920eed89ee7
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-01-05 15:30:38 +0800

    vue 13 biggie

--- js/vue.js
+++ js/vue.js
@@ -74,20 +74,3 @@ new Vue({
   }
 });
 
-  const app = Vue.createApp({
-   data() {
-    return {
-      manyFoods: [
-        'img_burrito.svg',
-        'img_salad.svg',
-        'img_cake.svg',
-        'img_soup.svg',
-        'img_fish.svg',
-        'img_pizza.svg',
-        'img_rice.svg'
-      ]
-    }
-   }
-  })
-  
-  app.mount('#app')
