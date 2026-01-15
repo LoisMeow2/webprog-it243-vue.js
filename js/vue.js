edf22cb57d87586d604b23776862313f3052ba4f
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-01-15 10:28:57 +0800

    app2

--- js/vue.js
+++ js/vue.js
@@ -74,7 +74,7 @@ new Vue({
   }
 });
 
-  const app = Vue.createApp({
+  const app2 = Vue.createApp({
    data() {
     return {
       manyFoods: [
@@ -90,4 +90,4 @@ new Vue({
    }
   })
   
-  app.mount('#app')
+  app2.mount('#app2')
