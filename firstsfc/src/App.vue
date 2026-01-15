09891419042742a0e2414815c4732ae3e5cb6065
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-01-15 06:25:54 +0000

    yasstem

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <h1>{{ message }}</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      message: 'havent you heard im the pisonet girl yass edittignging'
-    };
+<script></script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
   }
-};
-
-</script>
-
-<style></style>
+</style>
