530dc407ea6e89a605587e39aaa262ff8cc8353d
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-02-02 06:56:18 +0000

    whatevs check

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,32 +1,11 @@
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+  <template>
+    <h1>My Personal Profile Page</h1>
+    <food-item/>
+    <personal-profile/>
+    <comment-form/>
+    <comment/>
+    <rest-api/>
 
-  const countries = ref([])
+  </template>
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-
-<template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-
-  
-   <h1>Food</h1>
-  <personal-profile/> <br>
-  <food-item/> 
-  <food-item2/>
-  <food-item/> 
-  <food-item2/> 
-  <rest-api/> 
-</template>
-
-<style></style>
+  <script></script>
